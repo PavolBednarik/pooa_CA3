f46162d2bed98d7b8c319197479466d566e7757c
PavolBednarik <152331129+PavolBednarik@users.noreply.github.com>
2024-03-26 20:41:03 +0000

    Initial commit

---
+++ src/cms_ca3/DBConnector.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cms_ca3;
+
+/**
+ *
+ * @author pavol
+ */
+public class DBConnector {
+    protected final String DB_URL = "jdbc:mysql://localhost";
+    protected final String USER = "pooa2024";
+    protected final String PASSWORD = "pooa2024";
+}
