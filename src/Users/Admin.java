58e953d7b51e6ec0ad33ded5459f2f9fa6faa805
PavolBednarik <152331129+PavolBednarik@users.noreply.github.com>
2024-03-31 23:18:34 +0100

    in Admin class methods delete user add new user and modify user

--- src/Users/Admin.java
+++ src/Users/Admin.java
@@ -12,12 +12,62 @@ import java.util.Scanner;
  * @author pavol
  */
 public class Admin extends User {
+
     private final Scanner sc;
     private final DatabaseUserTable database;
-public Admin(String username, String password, Userrole role) {
-        super(username, password, ADMIN);
-         this.sc = new Scanner(System.in);
-         this.database = new DatabaseUserTable();
-}
 
+    public Admin(String username, String password, Userrole role) {
+        super(username, password, ADMIN);
+        this.sc = new Scanner(System.in);
+        this.database = new DatabaseUserTable();
+    }
+    // add new user method
+    public void addNewUser() {
+        System.out.println("Enter username:");
+        String username = sc.nextLine();
+
+        System.out.println("Enter password:");
+        String password = sc.nextLine();
+
+        System.out.println("Enter role (ADMIN, OFFICE, LECTURER):");
+        String roleString = sc.nextLine().toUpperCase();
+        Userrole role;
+        try {
+            role = Userrole.valueOf(roleString);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid role. Please enter one of: ADMIN, OFFICE, LECTURER");
+            return;
+        }
+        // adding new user to database
+        database.addNewUser(username, password, role);
+    }
+    // modify user
+    public void modifyUser() {
+        System.out.print("Enter the username of the user to modify: ");
+        String username = sc.nextLine();
+
+        System.out.print("Enter the new username: ");
+        String newUsername = sc.nextLine();
+
+        System.out.print("Enter the new password: ");
+        String newPassword = sc.nextLine();
+
+        System.out.print("Enter the new role (ADMIN, OFFICE, or LECTURER): ");
+        String newroleString = sc.nextLine().toUpperCase();
+        Userrole role;
+        try {
+            role = Userrole.valueOf(newroleString); // user role need to be enum 
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid role. Please enter one of: ADMIN, OFFICE, LECTURER");
+            return;
+        }
+        // updating user in database
+        database.modifyUser(username, newUsername, newPassword, role);
+    }
+    // deleting users
+    public void deleteUser() {
+        System.out.println("Enter the username of the user to delete:");
+        String username = sc.nextLine();
+        database.deleteUser(username);
+    }
 }
